ddcfe6ec16ec42409f7bdc18a790a1d7342fa9cf
igor <igor.viktorovich.ua@gmail.com>
2017-07-12 14:58:22 +0300

    start

---
+++ SuperTank/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperTank
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (e.KeyCode == Keys.Left)
+                Keyboard.Left = true;
+            else if (e.KeyCode == Keys.Right)
+                Keyboard.Right = true;
+            else if (e.KeyCode == Keys.Up)
+                Keyboard.Up = true;
+            else if (e.KeyCode == Keys.Down)
+                Keyboard.Down = true;
+            else if (e.KeyCode == Keys.Space)
+                Keyboard.Space = true;
+            else if (e.KeyCode == Keys.Enter)
+                Keyboard.Enter = true;
+            else if (e.KeyCode == Keys.Escape)
+                Keyboard.Escape = true;
+        }
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            base.OnKeyUp(e);
+
+            if (e.KeyCode == Keys.Left)
+                Keyboard.Left = false;
+            else if (e.KeyCode == Keys.Right)
+                Keyboard.Right = false;
+            else if (e.KeyCode == Keys.Up)
+                Keyboard.Up = false;
+            else if (e.KeyCode == Keys.Down)
+                Keyboard.Down = false;
+            else if (e.KeyCode == Keys.Space)
+                Keyboard.Space = false;
+            else if (e.KeyCode == Keys.Enter)
+                Keyboard.Enter = false;
+            else if (e.KeyCode == Keys.Escape)
+                Keyboard.Escape = false;
+        }
+    }
+}
---
+++ SuperTank/Keyboard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperTank
+{
+    /// <summary>
+    /// Состояние клавиатуры
+    /// </summary>
+    public struct Keyboard
+    {
+        public static bool Enter { get; set; }
+        public static bool Escape { get; set; }
+        public static bool Left { get; set; }
+        public static bool Right { get; set; }
+        public static bool Down { get; set; }
+        public static bool Up { get; set; }
+        public static bool Space { get; set; }
+    }
+}
