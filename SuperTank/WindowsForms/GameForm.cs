5edef3cc2235083422c950e2f1e8f48a5a2f94bd
igor <igor.viktorovich.ua@gmail.com>
2017-09-17 16:52:13 +0300

    update wcf

--- SuperTank/WindowsForms/GameForm.cs
+++ SuperTank/WindowsForms/GameForm.cs
@@ -12,20 +12,35 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.ServiceModel;
 using SuperTank.Audio;
+using System.Threading;
 
 namespace SuperTank.WindowsForms
 {
     public partial class GameForm : Form
     {
-        private IKeyboard keyboard;
+        private Game game;
+
+        private ServiceHost hostSound;
+        private ServiceHost hostSceneView;
+        private ServiceHost hostGameInfo;
+
+        private static ChannelFactory<IKeyboard> factoryKeyboard;
+
+        private SoundGame soundGame;
+        private SceneView sceneView;
         private ScrenGame screnGame;
         private static IViewSound viewSound;
 
-        public GameForm(SceneView sceneView, IViewSound sound)
+        private static bool wcfClose;
+
+        public GameForm()
         {
             InitializeComponent();
 
-            GameForm.viewSound = sound;
+            sceneView = new SceneView();
+            soundGame = new SoundGame();
+
+            GameForm.viewSound = soundGame;
             this.ClientSize = new Size(ConfigurationView.WindowClientWidth, ConfigurationView.WindowClientHeight);
 
             this.SuspendLayout();
@@ -33,16 +48,83 @@ namespace SuperTank.WindowsForms
             Controls.Add(screnGame);
             this.ResumeLayout(false);
 
-            this.Focus();
+            this.FormClosing += GameForm_FormClosing;
+            this.OpenHost();
+
+            ThreadPool.QueueUserWorkItem((s) =>
+            {
+                game = new Game();
+                game.Start();
+            });
+            this.OpenChenalFactory();
         }
+
+        private void GameForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!wcfClose)
+            {
+                e.Cancel = true;
+                soundGame.Dispose();
+
+                ThreadPool.QueueUserWorkItem(s =>
+                {
+                    game.Stop();
+                    game.CloseFactory();
+                    this.CloseChenalFactory();
+                    game.CloseHost();
+                    this.CloseHost();
+
+                    wcfClose = true;
+
+                    ((Form)sender).Invoke((MethodInvoker)delegate ()
+                    {
+                        ((Form)sender).Close();
+                    });
+                });
+            }
+        }
+
+        public void OpenHost()
+        {
+            hostSound = new ServiceHost(soundGame);
+            hostSound.CloseTimeout = TimeSpan.FromMilliseconds(0);
+            hostSound.AddServiceEndpoint(typeof(ISoundGame), new NetTcpBinding(), "net.tcp://localhost:9090/ISoundGame");
+            hostSound.Open();
+
+
+            hostSceneView = new ServiceHost(sceneView);
+            hostSceneView.CloseTimeout = TimeSpan.FromMilliseconds(0);
+            hostSceneView.AddServiceEndpoint(typeof(IRender), new NetTcpBinding(), "net.tcp://localhost:9090/IRender");
+            hostSceneView.Open();
+
+            hostGameInfo = new ServiceHost(screnGame);
+            hostGameInfo.CloseTimeout = TimeSpan.FromMilliseconds(0);
+            hostGameInfo.AddServiceEndpoint(typeof(IGameInfo), new NetTcpBinding(), "net.tcp://localhost:9090/IGameInfo");
+            hostGameInfo.Open();
+        }
+
+        public void OpenChenalFactory()
+        {
+            factoryKeyboard = new ChannelFactory<IKeyboard>(new NetTcpBinding(), "net.tcp://localhost:9090/IKeyboard");
+            Keyboard = factoryKeyboard.CreateChannel();
+        }
+
+        public void CloseChenalFactory()
+        {
+            factoryKeyboard.Close();
+        }
+
+        public void CloseHost()
+        {
+            hostSound.Close();
+            hostSceneView.Close();
+            hostGameInfo.Close();
+        }
+
 
         public static IViewSound Sound { get { return viewSound; } }
 
-        public IKeyboard Keyboard
-        {
-            get { return keyboard; }
-            set { keyboard = value; }
-        }
+        public IKeyboard Keyboard { get; set; }
 
         protected override void OnKeyDown(KeyEventArgs e)
         {
@@ -57,7 +139,7 @@ namespace SuperTank.WindowsForms
                 case Keys.Space:
                 case Keys.Enter:
                 case Keys.Escape:
-                    keyboard.KeyDown(e.KeyCode);
+                    Keyboard.KeyDown(e.KeyCode);
                     break;
             }
         }
@@ -74,7 +156,7 @@ namespace SuperTank.WindowsForms
                 case Keys.Space:
                 case Keys.Enter:
                 case Keys.Escape:
-                    keyboard.KeyUp(e.KeyCode);
+                    Keyboard.KeyUp(e.KeyCode);
                     break;
             }
         }
--- SuperTank/WindowsForms/Program.cs
+++ SuperTank/WindowsForms/Program.cs
@@ -12,13 +12,6 @@ namespace SuperTank.WindowsForms
 {
     static class Program
     {
-        private static ChannelFactory<IKeyboard> factoryKeyboard;
-        private static ServiceHost hostSound;
-        private static ServiceHost hostSceneView;
-        private static ServiceHost hostGameInfo;
-        private static Game game;
-        private static SoundGame soundGame;
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -27,69 +20,8 @@ namespace SuperTank.WindowsForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-
-            SceneView sceneView = new SceneView();
-            soundGame = new SoundGame();
-            GameForm formRender = new GameForm(sceneView, soundGame);
-            formRender.FormClosing += FormRender_FormClosing;
-
-            hostSound = new ServiceHost(soundGame);
-            hostSound.CloseTimeout = TimeSpan.FromMilliseconds(0);
-            hostSound.AddServiceEndpoint(typeof(ISoundGame), new NetTcpBinding(), "net.tcp://localhost:9090/ISoundGame");
-            hostSound.Open();
-
-
-            hostSceneView = new ServiceHost(sceneView);
-            hostSceneView.CloseTimeout = TimeSpan.FromMilliseconds(0);
-            hostSceneView.AddServiceEndpoint(typeof(IRender), new NetTcpBinding(), "net.tcp://localhost:9090/IRender");
-            hostSceneView.Open();
-
-            hostGameInfo = new ServiceHost(formRender);
-            hostGameInfo.CloseTimeout = TimeSpan.FromMilliseconds(0);
-            hostGameInfo.AddServiceEndpoint(typeof(IGameInfo), new NetTcpBinding(), "net.tcp://localhost:9090/IGameInfo");
-            hostGameInfo.Open();
-
-            ThreadPool.QueueUserWorkItem((s) =>
-            {  
-                game = new Game();
-                game.Start();
-            });
-
-            factoryKeyboard = new ChannelFactory<IKeyboard>(new NetTcpBinding(), "net.tcp://localhost:9090/IKeyboard");
-            formRender.Keyboard = factoryKeyboard.CreateChannel();
             
-            Application.Run(formRender);
-        }
-
-        private static bool wcfClose;
-        private static void FormRender_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            if (!wcfClose)
-            {
-                e.Cancel = true;
-                soundGame.Dispose();
-
-                ThreadPool.QueueUserWorkItem(s =>
-                {
-                    game.Stop();
-                    game.CloseFactory();
-
-                    factoryKeyboard.Close();
-                    hostSound.Close();
-                    hostSceneView.Close();
-                    hostGameInfo.Close();
-
-                    game.CloseHost();
-
-                    wcfClose = true;
-
-                    ((Form)sender).Invoke((MethodInvoker)delegate ()
-                    {
-                        ((Form)sender).Close();
-                    });
-                });
-            }
+            Application.Run(new GameForm());
         }
     }
 }
